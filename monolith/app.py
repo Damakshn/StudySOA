c8d9491bb0741693c9917f7e450748cfe1b42841
Damakshn <damakshon@gmail.com>
2019-10-28 14:44:35 +0800

    Всё написанное ранее переложено в Starting Начат учебный проект Runnerly

---
+++ monolith/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+from database import db, User
+from forms import UserForm
+from flask_wtf.csrf import CsrfProtect
+
+app = Flask(__name__)
+csrf = CsrfProtect()
+
+
+@app.route("/users")
+def users():
+    users = db.session.query(User)
+    return render_template("users.html", users=users)
+
+
+@app.route("/create_user", methods=["GET", "POST"])
+def create_user():
+    form = UserForm()
+    if request.method == "POST":
+        new_user = User()
+        form.populate_obj(new_user)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect("/users")
+    return render_template("create_user.html", form=form)
+
+
+if __name__ == "__main__":
+    db.init_app(app)
+    csrf.init_app(app)
+    db.create_all(app=app)
+    app.run()
