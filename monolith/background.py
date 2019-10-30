6ebce6333098af41c2a8d60fa805c87a46c8748d
Damakshn <damakshon@gmail.com>
2019-10-30 16:17:04 +0800

    Добавлены модели, добавлен файл для celery, обновлён requirements.txt

---
+++ monolith/background.py
@@ -0,0 +1,3 @@
+from celery import Celery
+from stravalib import Client
+from monolith.database import db, User, Run
--- monolith/database.py
+++ monolith/database.py
@@ -16,3 +16,64 @@ class User(db.Model):
     max_hr = db.Column(db.Integer)
     rest_hr = db.Column(db.Integer)
     vo2max = db.Column(db.Numeric(4, 2))
+    is_anonymous = False
+
+    def __init__(self, *args, **kw):
+        super(User, self).__init__(*args, **kw)
+        self._authenticated = False
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    @property
+    def is_authenticated(self):
+        return self._authenticated
+
+    def authenticate(self, password):
+        checked = check_password_hash(self.password, password)
+        self._authenticated = checked
+        return self._authenticated
+
+    def get_id(self):
+        return self.id
+
+
+class Gender(enum.Enum):
+    # as per ISO 5218
+    not_known = '0'
+    male = '1'
+    female = '2'
+    not_applicable = '9'
+
+
+class Run(db.Model):
+    __tablename__ = 'run'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Unicode(128))
+    strava_id = db.Column(db.Integer)
+    distance = db.Column(db.Float)
+    start_date = db.Column(db.DateTime)
+    elapsed_time = db.Column(db.Float)
+    average_speed = db.Column(db.Float)
+    average_heartrate = db.Column(db.Float)
+    total_elevation_gain = db.Column(db.Float)
+    runner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    runner = relationship('User', foreign_keys='Run.runner_id')
+
+
+class Race(db.Model):
+    __tablename__ = 'race'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Unicode(128))
+    date = db.Column(db.Date)
+    distance = db.Column(db.Float)
+    location = db.Column(db.Unicode(128))
+
+
+class Plan(db.Model):
+    __tablename__ = 'plan'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Unicode(128))
+    date = db.Column(db.Date)
+    distance = db.Column(db.Float)
+    location = db.Column(db.Unicode(128))
